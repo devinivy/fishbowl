8003c4f864ba548fd458537f8dbe833ccc193375
Devin Ivy <devin@bigroomstudios.com>
2020-03-24 23:33:18 -0400

    Allow join box to be closed

--- packages/frontend/src/routes/game/components/InitializedGame.js
+++ packages/frontend/src/routes/game/components/InitializedGame.js
@@ -1,4 +1,5 @@
 const React = require('react');
+const { useState, useCallback } = require('react');
 const { useTheme } = require('@material-ui/core/styles');
 const { default: useMediaQuery } = require('@material-ui/core/useMediaQuery');
 const { default: Styled } = require('styled-components');
@@ -8,6 +9,8 @@ const { default: TextField } = require('@material-ui/core/TextField');
 const { default: Divider } = require('@material-ui/core/Divider');
 const { default: Paper } = require('@material-ui/core/Paper');
 const { default: Typography } = require('@material-ui/core/Typography');
+const { default: IconButton } = require('@material-ui/core/IconButton');
+const { default: CloseIcon } = require('@material-ui/icons/Cancel');
 const { default: LightGreen } = require('@material-ui/core/colors/lightGreen');
 const GameListItem = require('../../../components/GameListItem');
 const SubmitWordsForm = require('../../../components/SubmitWordsForm');
@@ -20,7 +23,10 @@ module.exports = function InitializedGame({ game }) {
     const theme = useTheme();
     const smUp = useMediaQuery(theme.breakpoints.up('sm'));
 
-    const { GreenButton } = internals;
+    const [showJoin, setShowJoin] = useState(true);
+    const handleCloseJoin = useCallback(() => setShowJoin(false), []);
+
+    const { GreenButton, CloseIconButton } = internals;
     const allPlayersReady = game.players.every(({ status }) => status === 'ready');
 
     return (
@@ -35,40 +41,50 @@ module.exports = function InitializedGame({ game }) {
                 <Box flexGrow={2}>
                     <GameListItem component='div' game={game} />
                 </Box>
-                <Box
-                    display='flex'
-                    px={2}
-                    py={!game.me && { xs: 2, sm: 1 }}
-                    my={game.me ? 1 : 0}
-                    bgcolor={!game.me && 'secondary.main'}
-                    alignItems='center'
-                    justifyContent='center'
-                    flexBasis={!game.me && 230}
-                    flexGrow={{ xs: 1, sm: 0 }}
-                    flexShrink={1}
-                    borderRadius={{ xs: 0, sm: 'borderRadius' }}
-                >
-                    {game.me && (
-                        <GreenButton
-                            fullWidth
-                            disabled={!allPlayersReady}
-                            title={!allPlayersReady && 'not all players are ready'}
-                            variant='contained'
-                        >
-                            begin
-                        </GreenButton>
-                    )}
-                    {!game.me && (
-                        <>
-                            <Box mr={2}>
-                                <Button variant='outlined'>join</Button>
-                            </Box>
-                            <Box>
-                                <TextField fullWidth placeholder='nickname' />
-                            </Box>
-                        </>
-                    )}
-                </Box>
+                {game.me || showJoin && (
+                    <Box
+                        display='flex'
+                        position='relative'
+                        px={2}
+                        py={!game.me && { xs: 2, sm: 1 }}
+                        my={game.me ? 1 : 0}
+                        bgcolor={!game.me && 'secondary.main'}
+                        alignItems='center'
+                        justifyContent='center'
+                        flexBasis={!game.me && 230}
+                        flexGrow={{ xs: 1, sm: 0 }}
+                        flexShrink={1}
+                        borderRadius={{ xs: 0, sm: 'borderRadius' }}
+                    >
+                        {game.me && (
+                            <GreenButton
+                                fullWidth
+                                disabled={!allPlayersReady}
+                                title={!allPlayersReady && 'not all players are ready'}
+                                variant='contained'
+                            >
+                                begin
+                            </GreenButton>
+                        )}
+                        {!game.me && (
+                            <>
+                                <CloseIconButton
+                                    title='spectate'
+                                    size='small'
+                                    onClick={handleCloseJoin}
+                                >
+                                    <CloseIcon />
+                                </CloseIconButton>
+                                <Box mr={2}>
+                                    <Button variant='outlined'>join</Button>
+                                </Box>
+                                <Box>
+                                    <TextField fullWidth placeholder='nickname' />
+                                </Box>
+                            </>
+                        )}
+                    </Box>
+                )}
             </Box>
             <Box mb={{ sm: 2 }}>
                 <Divider />
@@ -95,5 +111,15 @@ internals.GreenButton = Styled(Button)`
     background-color: ${LightGreen[500]};
     :hover {
         background-color: ${LightGreen[500]};
+    }
+`;
+
+internals.CloseIconButton = Styled(IconButton)`
+    position: absolute;
+    top: -${({ theme }) => theme.spacing(1)}px;
+    right: -${({ theme }) => theme.spacing(1)}px;
+    svg {
+        width: .75em;
+        height: .75em;
     }
 `;
