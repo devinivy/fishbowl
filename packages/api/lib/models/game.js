5c8cb09ac192abfd67739a2bb742862691ba1470
Devin Ivy <devin@bigroomstudios.com>
2020-08-30 01:43:04 -0400

    Update to hapi v20 and other misc dep updates

--- packages/api/lib/models/game.js
+++ packages/api/lib/models/game.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const Schwifty = require('schwifty');
-const Joi = require('@hapi/joi');
+const Joi = require('joi');
 const Shortid = require('shortid');
 
 module.exports = class Game extends Schwifty.Model {
